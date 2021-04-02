b7ff47d49b44a39ab8a149f89719147216ba535a
sean goudie <sgoudie77@hotmail.com>
2021-04-02 11:55:22 -0700

    Search and covnerter buttons added, C/F converter linked to hi/low, backgorund weather image displays

--- readme.md
+++ readme.md
@@ -22,7 +22,8 @@ This weather app allows users to display the current temperature and weather con
 
 The code for this weather app was based on YouTube tutorials by Dev Ed and Tyler Potts, which will be enhanced by changing some of the styles and adding additional functionality, such as:
 
-- default text displays
-- a F/C degree converter
+- default weather display based on user's location
+- add a button for a C/F degree converter
 - images that change according to the current weather
 - weather icons to match the current weather
+- background image to change according to search term
--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const api = {
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
+//search button linked to search bar
+document.querySelector('header button').addEventListener('click', function () {
+    getResults(searchbox.value);
+});
+
 function setQuery(e) {
     if (e.keyCode == 13) {
         getResults(searchbox.value);
@@ -27,19 +32,62 @@ function displayResults (weather) {
     date.innerText = dateBuilder(now);
 
     let temp = document.querySelector('.current .temp');
+    let state; 
+
+    //default
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+    state = 'celsuis';
+
+    //display background image to match the search term
+    //document.body.style.background = "url('https://source.unsplash.com/1600x900/?" + city + "')";
+
     //formula to change celsius to fahrenheight
-    let tempImperial = (temp + 32) * (9 / 5);
-    //change temp from celsius to fahrenheit
+    let tempImperial = (weather.main.temp + 32) * (9 / 5);
+    //formula to change hi/low celsius to fahrenheit
+    let tempImperialHi = (weather.main.temp_max +32) * (9 /5);
+    let tempImperialLow = (weather.main.temp_min +32) * (9 /5);
+    //change temp and hi/low from celsius to fahrenheit
     temp.addEventListener('click', () => {
-        temp.innerHTML = `${Math.round({tempImperial})}<span>°f</span>`
+        if (state != 'fahrenheit') {
+            temp.innerHTML = `${Math.round(tempImperial)}<span>°F</span>`;
+            hilow.innerText = `${Math.round(tempImperialLow)}°F / ${Math.round(tempImperialHi)}°F`;
+            state = 'fahrenheit';
+        } else if (state != 'celsuis') {
+            temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+            hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+            state = 'celsius';
+        }   
     })
+
+    //button to convert c/f
+    // document.querySelector('.current button').addEventListener('click' function () {
+    //     if (state != 'fahrenheit') {
+    //         temp.innerHTML = `${Math.round(tempImperial)}<span>°F</span>`;
+    //         hilow.innerText = `${Math.round(tempImperialLow)}°F / ${Math.round(tempImperialHi)}°F`;
+    //         state = 'fahrenheit';
+    //     } else if (state != 'celsuis') {
+    //         temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+    //         hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+    //         state = 'celsius';
+    //     }
+    // })
+
+    //weather icons
+    let weather_icon = document.querySelector('.current .icon');
+    weather_icon = weather.weather[0].icon;
+    //document.querySelector('.current .icon').src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    //const {icon} = weather.weather[0].icon;
+    //weather_icon.innerHTML = `<img src="icons/${icon}.png">;`
+    //weather_icon.innerHTML = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    //weather_icon.innerHTML = `{"http://openweathermap.org/img/wn/" + ${icon} + "@2x.png"};`
+    //.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    //document.querySelector('.current .icon').src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
     let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
 }
 
 function dateBuilder (d) {
