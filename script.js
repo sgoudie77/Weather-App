1bdd1aa61d40012dde6901b1027c35d5bb104b21
sean goudie <sgoudie77@hotmail.com>
2021-04-09 11:18:27 -0700

    Added responsive design, linked the coverter btn

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ const api = {
     base: "https://api.openweathermap.org/data/2.5/"
 }
 
+// Seet variable for the searchbox input
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
-//search button linked to search bar
+// Search button linked to search bar
 document.querySelector('header button').addEventListener('click', function () {
     getResults(searchbox.value);
 });
 
+// Get results when enter key is pressed
 function setQuery(e) {
     if (e.keyCode == 13) {
         getResults(searchbox.value);
     }
 }
 
+// Fetch request to the  API tp get the data
 function getResults (query) {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`).then(weather => {
         return weather.json();
     }).then(displayResults);
 }
 
+// Set variables to display the results
 function displayResults (weather) {
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
@@ -34,19 +38,19 @@ function displayResults (weather) {
     let temp = document.querySelector('.current .temp');
     let state; 
 
-    //default
+    // Default display and state
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
     state = 'celsius';
 
-    //display background image to match the search term
+    // Display background image to match the search term
     document.body.style.background = "url('https://source.unsplash.com/1600x900/?" + weather.weather[0].main + "')";
 
-    //formula to change celsius to fahrenheight
+    // Formula to change celsius to fahrenheight
     let tempImperial = (weather.main.temp + 32) * (9 / 5);
-    //formula to change hi/low celsius to fahrenheit
+    // Formula to change hi/low celsius to fahrenheit
     let tempImperialHi = (weather.main.temp_max +32) * (9 /5);
     let tempImperialLow = (weather.main.temp_min +32) * (9 /5);
-    //change temp and hi/low from celsius to fahrenheit
+    // Change temp and hi/low from celsius to fahrenheit
     temp.addEventListener('click', () => {
         if (state != 'fahrenheit') {
             temp.innerHTML = `${Math.round(tempImperial)}<span>°F</span>`;
@@ -59,20 +63,20 @@ function displayResults (weather) {
         }   
     })
 
-    //button to convert c/f
-    // document.querySelector('.current button').addEventListener('click' function () {
-    //     if (state != 'fahrenheit') {
-    //         temp.innerHTML = `${Math.round(tempImperial)}<span>°F</span>`;
-    //         hilow.innerText = `${Math.round(tempImperialLow)}°F / ${Math.round(tempImperialHi)}°F`;
-    //         state = 'fahrenheit';
-    //     } else if (state != 'celsius') {
-    //         temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
-    //         hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
-    //         state = 'celsius';
-    //     }
-    // })
+    // Button to convert c/f
+    document.querySelector('.current button').addEventListener('click', function () {
+        if (state != 'fahrenheit') {
+            temp.innerHTML = `${Math.round(tempImperial)}<span>°F</span>`;
+            hilow.innerText = `${Math.round(tempImperialLow)}°F / ${Math.round(tempImperialHi)}°F`;
+            state = 'fahrenheit';
+        } else if (state != 'celsius') {
+            temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+            hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+            state = 'celsius';
+        }
+    })
 
-    //weather icons
+    // Weather icons
     let icon = weather.weather[0].icon;
     document.querySelector('.current .icon').src = "http://openweathermap.org/img/wn/" + icon + ".png";
 
@@ -83,6 +87,7 @@ function displayResults (weather) {
     hilow.innerText = `Hi ${Math.round(weather.main.temp_min)}°C / Low ${Math.round(weather.main.temp_max)}°C`;
 }
 
+// Set the date to display
 function dateBuilder (d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -96,7 +101,7 @@ function dateBuilder (d) {
 }
 
 
-//OpenCage Geocoding API
+// OpenCage Geocoding API - code from the site
 
 let geocode = {
     reverseGeocode: function (latitude, longitude) {
